60e9500dcb0e292c5b2e464cc71324d137a37b52
saksham <sharma.saksham137@gmail.com>
2023-08-26 14:05:21 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var btntranslate = document.querySelector("#btn-translate")
+
 var txtInput = document.querySelector("#txt-input")
-   console.log(txtInput)
+
+   var outputDiv = document.querySelector("#output")
+   outputDiv.innerText = "saksham sharma " + txtInput.value;
+
 
 btntranslate.addEventListener("click", function clickEventHandler(){
     console.log("clicked!")
+    console.log("input", txtInput.value);
 })
